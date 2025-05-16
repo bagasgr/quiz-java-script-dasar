e4b31589c9a20d4300541673173221a004951af9
Bagas Gilang Ramadhan <bagasiayin205@gmail.com>
2025-05-16 21:00:44 +0700

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = 3001;
 const ROOMS_FILE = path.join(__dirname, 'rooms.json');
 
 app.use(express.static('public')); // folder 'public' untuk client.js & index.html
